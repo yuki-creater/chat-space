e845e9a15a0505f9b60aad50ed44ae77ac414cfc
yuki-creater <65445435+yuki-creater@users.noreply.github.com>
2020-06-07 16:32:24 +0900

    surver side

--- db/migrate/20200519013702_add_index_to_scores.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToScores < ActiveRecord::Migration[6.0]
-  def change
-    add_index :scores,  :name
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,34 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_014323) do
-
-  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
-    t.text "text"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "scores", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.integer "score"
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["name"], name: "index_scores_on_name"
-    t.index ["user_id"], name: "index_scores_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2020_06_07_072501) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "email"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["name", "email"], name: "index_users_on_name_and_email"
+    t.index ["name"], name: "index_users_on_name", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "scores", "users"
 end
