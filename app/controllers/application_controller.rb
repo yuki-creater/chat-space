712525af4237f3a78dca0762102381692e9ce2f2
yuki-creater <65445435+yuki-creater@users.noreply.github.com>
2020-06-15 19:04:27 +0900

    surver side3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # application.controllerにbefore_actionをしているので全てのアクションの実行前の一番最初に実行される
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # deviseを導入していることで devise_parameter_sanitizer.permitが使えるようになっている（これはストロングパラメーターに該当する概念）
+    # サインアップ時に入力された「name」キーの内容の保存を許可している
   end
   module ChatSpace
     class Application < Rails::Application
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
 
+  has_many :group_users
+  has_many :groups, through: :group_users
+  has_many :messages
+
   validates :name, presence: true, uniqueness: true
 end
