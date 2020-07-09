4aa907172b150c69852ba908b8fded2a9daa8284
yuki-creater <65445435+yuki-creater@users.noreply.github.com>
2020-07-09 18:41:52 +0900

    fixed

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,6 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
+  server '175.41.226.118', user: 'ec2-user', roles: %w{app db web}
 
 
 
